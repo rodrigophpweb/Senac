ce631035cb1d6fd0b1a92c5a91531f689b9d2d5b
Rodrigo <rodrigo.mct@gmail.com>
2020-02-22 17:50:47 -0300

    removendo folder includes

--- includes/fotos.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-12 col-lg-6 wow animated fadeInRight fotos">
-    <div class="masked">
-        <img src="assets/images/laboratorio.jpg" alt="Laboratório"  />
-    </div>
-
-    <div class="masked">
-        <img src="assets/images/auditorio.jpg" alt="Auditório"  />
-    </div>
-</div>
--- includes/styles.php
+++
@@ -1,5 +0,0 @@
-<!-- Bootstrap, Style e Animate -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    
